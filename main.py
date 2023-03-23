1992c37dc5c4cb0459c66bcc4345e2255fc4329f
antonio <xirator42@gmail.com>
2023-03-22 22:39:17 -0300

    teste 1

--- main.py
+++
@@ -1,9 +0,0 @@
-import metodos
-
-opcao = 0;
-isPrimeiraExecucao = True
-
-while opcao != 0 or isPrimeiraExecucao:
-    isPrimeiraExecucao = False
-    opcao = metodos.printarMenu()
-    print("O programa foi encerrado!!!")
