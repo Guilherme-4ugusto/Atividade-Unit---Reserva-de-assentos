9d78643266185f78a73820fcbfe9c8c2b247c09f
Guilherme <guilhermeabscontato@gmail.com>
2023-03-22 21:13:09 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import metodos
+
+opcao = 0;
+isPrimeiraExecucao = True
+
+while opcao != 0 or isPrimeiraExecucao:
+    isPrimeiraExecucao = False
+    opcao = metodos.printarMenu()
---
+++ metodos.py
@@ -0,0 +1,5 @@
+def printarMenu():
+    print("Opção 1:")
+    print("Opção 2:")
+    print("Opção 3:")
+    return int(input("Informe uma opção:"))
